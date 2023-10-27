d09f7a5ff389191ff5acf24bbba5a68c33307d98
SheGAll <289789948@qq.com>
2023-10-27 19:22:07 -0400

    rewrite Host.cpp add stopwatch.h

--- hw7/Host.cpp
+++ hw7/Host.cpp
@@ -3,6 +3,7 @@
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY 1
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define STAGES (4)
 
 #include <CL/cl2.hpp>
 #include <cstdint>
@@ -14,17 +15,30 @@
 
 #include "Pipeline.h"
 #include "Utilities.h"
+#include "Stopwatch.h"
+#include "EventTimer.h"
 
 int main(int argc, char *argv[])
 {
-    EventTimer timer1, timer2;
-    timer1.add("Main program");
+    unsigned char *Input_data = (unsigned char *)malloc(FRAMES * FRAME_SIZE);
+    unsigned char *Temp_data[STAGES - 1];
+    unsigned char *Output_data = (unsigned char *)malloc(MAX_OUTPUT_SIZE);
+    for (int Stage = 0; Stage < STAGES - 1; Stage++){
+        Temp_data[Stage] = (unsigned char *)malloc(FRAME_SIZE);
+        if (Temp_data[Stage] == NULL)
+            Exit_with_error("malloc failed at main for Temp_data");
+    }
+    std::cout << "Loading Input.bin" << std::endl;
+    Load_data(Input_data);
+    stopwatch time_scale;
+    stopwatch time_filter;
+    stopwatch time_differentiate;
+    stopwatch time_compress;
+    stopwatch total_time;
+    int Size = 0;
 
-    std::cout << "Running " << FRAMES << " iterations of " << " task" << std::endl;
-    // ------------------------------------------------------------------------------------
     // Step 1: Initialize the OpenCL environment
-     // ------------------------------------------------------------------------------------
-    timer2.add("OpenCL Initialization");
+    std::cout << "Initializing OpenCL environment" << std::endl;
     cl_int err;
     std::string binaryFile = argv[1];
     unsigned fileBufSize;
@@ -36,118 +50,74 @@ int main(int argc, char *argv[])
     cl::Program::Binaries bins{{fileBuf, fileBufSize}};
     cl::Program program(context, devices, bins, NULL, &err);
     cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &err);
-    cl::Kernel krnl_filter(program, "Filter_HW", &err);
+    cl::Kernel Filter_HW(program, "Filter_HW", &err);
 
-    // ------------------------------------------------------------------------------------
-    // Step 2: Create buffers and initialize test values
-    // ------------------------------------------------------------------------------------
-    timer2.add("Allocate contiguous OpenCL buffers");
+    // Step 2: Create OpenCL buffers and map them to host buffers
+    std::cout << "Initializing OpenCL buffers" << std::endl;
+    size_t elements_per_iteration = FRAME_SIZE;
+    size_t bytes_per_iteration = elements_per_iteration * sizeof(unsigned char);
+    cl::Buffer Temp_0_buf;
+    cl::Buffer Temp_1_buf;
+    Temp_0_buf = cl::Buffer(context, CL_MEM_READ_ONLY, bytes_per_iteration, NULL, &err);
+    Temp_1_buf = cl::Buffer(context, CL_MEM_WRITE_ONLY, bytes_per_iteration, NULL, &err);
+    Temp_data[0] = (unsigned char*)q.enqueueMapBuffer(Temp_0_buf, CL_TRUE, CL_MAP_WRITE, 0, bytes_per_iteration);
+    Temp_data[1] = (unsigned char*)q.enqueueMapBuffer(Temp_1_buf, CL_TRUE, CL_MAP_READ, 0, bytes_per_iteration);
 
-    size_t Input_buf_size = SCALED_FRAME_HEIGHT * SCALED_FRAME_WIDTH;
-    size_t Output_buf_size = OUTPUT_FRAME_HEIGHT * OUTPUT_FRAME_WIDTH;
-
-    cl::Buffer Input_buf[FRAMES];
-    cl::Buffer Output_buf[FRAMES];
- 
-    for(int i = 0; i < FRAMES; i++)
-    {
-        Input_buf[i] = cl::Buffer(context, CL_MEM_READ_ONLY, Input_buf_size, NULL, &err);
-        Output_buf[i] = cl::Buffer(context, CL_MEM_WRITE_ONLY, Output_buf_size, NULL, &err);
+    // Step 3: Run the compression flow on host + kernel
+    td::cout << "Compressing " << FRAMES << " frames" << std::endl;
+    std::vector<cl::Event> write_events, exec_events, read_events;
+    cl::Event write_ev, exec_ev, read_ev;
+    for (int Frame = 0; Frame < FRAMES; Frame++){
+        total_time.start();
+        //Scale on host
+        time_scale.start();
+        Scale_SW(Input_data + Frame * FRAME_SIZE, Temp_data[0]);
+        time_scale.stop();
+        //Filter on FPGA kernel
+        time_filter.start();
+        Filter_HW.setArg(0, Temp_0_buf);
+        Filter_HW.setArg(1, Temp_1_buf);
+        q.enqueueMigrateMemObjects({Temp_0_buf}, 0 /* 0 means from host*/, NULL, &write_ev);
+        write_events.push_back(write_ev);
+        q.enqueueTask(Filter_HW, &write_events, &exec_ev);
+        exec_events.push_back(exec_ev);
+        write_events.pop_back();
+        q.enqueueMigrateMemObjects({Temp_1_buf}, CL_MIGRATE_MEM_OBJECT_HOST, &exec_events, &read_ev);
+        read_events.push_back(read_ev);
+        exec_events.pop_back();
+        read_events[0].wait();
+        read_events.pop_back();
+        time_filter.stop();
+        //Differentiate on host
+        time_differentiate.start();
+        Differentiate_SW(Temp_data[1], Temp_data[2]);
+        time_differentiate.stop();
+        //Compression on host
+        time_compress.start();
+        Size = Compress_SW(Temp_data[2], Output_data);
+        time_compress.stop();
+        total_time.stop();
     }
-
-    unsigned char *Output_Scale[FRAMES], 
-                  *Input_Differentiate[FRAMES], *Output_Differentiate[FRAMES], 
-                  *Input_Compress[FRAMES];
-    int Result_size;
-
-    unsigned char *Input_Scale = (unsigned char*)malloc(INPUT_FRAME_WIDTH * INPUT_FRAME_HEIGHT * sizeof(unsigned char));
-    unsigned char *Output_Compress = (unsigned char*)malloc(OUTPUT_FRAME_WIDTH * OUTPUT_FRAME_HEIGHT * sizeof(unsigned char));
-
-    for (int i = 0; i < FRAMES; i++){
-        Input_Compress[i] = Output_Differentiate[i];
-    }
-
-    for(int i = 0; i < FRAMES; i++)
-    {
-        Output_Scale[i] = (unsigned char*)q.enqueueMapBuffer(Input_buf[i], CL_TRUE, CL_MAP_WRITE, 0, Input_buf_size);
-        Input_Differentiate[i] = (unsigned char*)q.enqueueMapBuffer(Output_buf[i], CL_TRUE, CL_MAP_READ, 0, Output_buf_size);
-    }
-
-    timer2.add("Populating input for Scale");
-    Load_data(Input_Scale);
-
-    // ------------------------------------------------------------------------------------
-    // Step 3: Start computing
-    // ------------------------------------------------------------------------------------
-
-    //--- define flags for kernel
-    std::vector<cl::Event> write_done(FRAMES);
-    std::vector<cl::Event> write_waitlist;
-    std::vector<std::vector<cl::Event>> execute_waitlist(FRAMES);
-    std::vector<cl::Event> execute_done(FRAMES);
-    std::vector<std::vector<cl::Event>> read_waitlist(FRAMES);
-    std::vector<cl::Event> read_done(FRAMES);
-
-    timer2.add("Running the computation");
-    for (int i = 0; i < FRAMES; i++){  //start computation
-        Scale_SW(Input_Scale + i * FRAME_SIZE, Output_Scale[i]);
-        //--------------------------------kernel computation --------------------------------
-        krnl_filter.setArg(0, Input_buf[i]);
-        krnl_filter.setArg(1, Output_buf[i]);
-
-        if (i == 0){
-            q.enqueueMigrateMemObjects({Input_buf[i]}, 0 /* 0 means from host*/, NULL, &write_done[i]);
-            write_waitlist.push_back(write_done[i]);
-        } else{
-            q.enqueueMigrateMemObjects({Input_buf[i]}, 0 /* 0 means from host*/, &write_waitlist, &write_done[i]);
-            write_waitlist.push_back(write_done[i]);
-        }
-
-        execute_waitlist[i].push_back(write_done[i]);
-        q.enqueueTask(krnl_filter, &execute_waitlist[i], &execute_done[i]);
-
-        read_waitlist[i].push_back(execute_done[i]);
-        q.enqueueMigrateMemObjects({Output_buf[i]}, CL_MIGRATE_MEM_OBJECT_HOST, &read_waitlist[i], &read_done[i]);
-        read_waitlist[i+1].push_back(read_done[i]);
-        //--------------------------------kernel computation --------------------------------
-        cl_int read_status = read_done[i].getInfo<CL_EVENT_COMMAND_EXECUTION_STATUS>();
-        if (read_status == CL_COMPLETE) {
-            Differentiate_SW(Input_Differentiate[i], Output_Differentiate[i]);
-            Result_size = Compress_SW(Input_Compress[i], Output_Compress);
-        }
-    } //end computation
-
+    q.enqueueUnmapMemObject(Temp_0_buf, Temp_data[0]);
+    q.enqueueUnmapMemObject(Temp_1_buf, Temp_data[1]);
     q.finish();
+    std::cout << "Total latency of Scale is: " << time_scale.latency() << " ns." << std::endl;
+    std::cout << "Total latency of Filter is: " << time_filter.latency() << " ns." << std::endl;
+    std::cout << "Total latency of Differentiate is: " << time_differentiate.latency() << "ns." << std::endl;
+    std::cout << "Total latency of Compress is: " << time_compress.latency() << " ns." << std::endl;
+    std::cout << "Total time taken by the loop is: " << total_time.latency() << " ns." << std::endl;
+    std::cout << "---------------------------------------------------------------" << std::endl;
+    std::cout << "Average latency of Scale per loop iteration is: " << time_scale.avg_latency() << " ns." << std::endl;
+    std::cout << "Average latency of Filter per loop iteration is: " << time_filter.avg_latency() << " ns." << std::endl;
+    std::cout << "Average latency of Differentiate per loop iteration is: " << time_differentiate.avg_latency() << " ns." << std::endl;
+    std::cout << "Average latency of Compress per loop iteration is: " << time_compress.avg_latency() << " ns." << std::endl;
+    std::cout << "Average latency of each loop iteration is: " << total_time.avg_latency() << " ns." << std::endl;
+    Store_data("Output.bin", Output_data, Size);
+    Check_data(Output_data, Size);
 
-    // ------------------------------------------------------------------------------------
-    // Step 4: Compare Results
-    // ------------------------------------------------------------------------------------
-    timer2.add("Writing compressed results to output_fpga.bin");
-    Store_data("Output.bin", Output_Compress, Result_size);
-
-    timer2.add("Check results with Golden.bin");
-    Check_data(Output_Compress, Result_size);
-    timer2.finish();
-    // ------------------------------------------------------------------------------------
-    // Step 5: Release Allocated Resources
-    // ------------------------------------------------------------------------------------
-    std::cout << "--------------- Key execution times ---------------"
-    << std::endl;
-    timer2.print();
-
-    timer1.finish();
-    std::cout << "--------------- Total time ---------------"
-    << std::endl;
-    timer1.print();
-
-    for (int i = 0; i < FRAMES; i++){
-        free(Output_Scale[i]);
-        free(Input_Differentiate[i]);
-        free(Output_Differentiate[i]);
-        free(Input_Compress[i]);
+    for (int i = 2; i < STAGES - 1; i++)
+        free(Temp_data[i]);
+    free(Input_data);
+    free(Output_data);
+    return EXIT_SUCCESS;
     }
-    free(Input_Scale);
-    free(Output_Compress);
-
-    return 0;
-}
