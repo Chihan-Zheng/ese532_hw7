7635d72bf9160d0e2748c8cab44e00030a01b5d6
Chihan-Zheng <charleszheng6778@gmail.com>
2023-10-27 12:36:06 -0400

    Update Host.cpp

--- hw7/Host.cpp
+++ hw7/Host.cpp
@@ -4,7 +4,6 @@
 #define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY 1
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 
-#include "EventTimer.h"
 #include <CL/cl2.hpp>
 #include <cstdint>
 #include <cstdlib>
